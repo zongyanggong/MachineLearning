679f81e912fc7ebfd5348b87fa32278cbd7334d8
zongyanggong <zongyanggong@gmail.com>
2022-12-31 13:54:48 -0500

    multipage App

---
+++ Steamlit/multipage_app/1_😀_Homepage.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Mutipage App',
+    page_icon="👋"
+)
+
+st.title('Main Page')
+
+st.sidebar.title('Navigation')
+
+st.sidebar.success('Select a page above.')
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] =""
+
+my_input = st.text_input('Input a text heare', st.session_state['my_input'])
+submit = st.button('Submit')
+if submit:
+    st.session_state['my_input'] = my_input
+    st.write('You have entered: ' + my_input)
+st.sidebar.header('Home sidebar')
---
+++ Steamlit/multipage_app/pages/2_🎭_Projects.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title('Projects')
+
+st.write("you have entered: ", st.session_state['my_input'])
+st.sidebar.header('project sidebar')
